c81314d373486f629266a74836156c9b829b97da
Lucia Bentancor <lucia@192.168.1.22>
2025-08-20 17:02:18 -0300

    comision fiscal y cuerpo tecnico

--- script/script-swiper.js
+++ script/script-swiper.js
@@ -36,3 +36,41 @@ var swiper_memorias = new Swiper(".swiper-memorias", {
     }
   }
 });
+
+var swiper_tecnicos = new Swiper(".swiper-tecnicos", {
+  slidesPerView: 5,
+  spaceBetween: 30,
+  centeredSlides: true,
+  slideToClickedSlide: true,
+  preventClicks: true,
+  roundLengths: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true
+  },
+
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10
+    },
+    480: {
+      slidesPerView: 1.5,
+      spaceBetween: 20
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 25
+    },
+    1024: {
+      slidesPerView: 3.5,
+      spaceBetween: 30
+    },
+    1280: {
+      slidesPerView: 5,
+      spaceBetween: 30
+    }
+  }
+});
